616da713e0c595a086a60e364e17167393b5fe58
Sam Yang <samyang.au@gmail.com>
2020-05-28 03:05:44 +1000

    fix uuid issue

--- src/platform.ts
+++ src/platform.ts
@@ -65,7 +65,7 @@ export class HomebridgeRemoteControlPlatform implements DynamicPlatformPlugin {
             // generate a unique id for the accessory this should be generated from
             // something globally unique, but constant, for example, the device serial
             // number or MAC address
-            const uuid = device.name
+            const uuid = this.api.hap.uuid.generate(device.name);
 
             // see if an accessory with the same uuid has already been registered and restored from
             // the cached devices we stored in the `configureAccessory` method above
